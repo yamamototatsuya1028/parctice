826613eaf50e58b1e9c4bb6daa7d63221c478bac
yamamototatsuya <yamamoto.tatsuya@xchange.jp>
2019-07-29 17:35:36 +0900

    add comment

--- iOS/RSSReader/RSSReader/Common/Extension/UIView+.swift
+++ iOS/RSSReader/RSSReader/Common/Extension/UIView+.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 extension UIView {
+    // 名前を表示できるようにしている。UITableViewCellでUINib(name: ) のところを簡単にしたいから。
     var className: String {
         return String(describing: type(of: self)) // ClassName
     }
