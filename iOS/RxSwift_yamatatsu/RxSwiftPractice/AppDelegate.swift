d3ae2c094ac202363134a3a7d161b13dc242bd3d
yamamototatsuya <yamamoto.tatsuya@xchange.jp>
2019-07-30 11:44:55 +0900

    add RxViewController

--- iOS/RxSwift_yamatatsu/RxSwiftPractice/AppDelegate.swift
+++ iOS/RxSwift_yamatatsu/RxSwiftPractice/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        let navi = UINavigationController(rootViewController: ViewController())
+        let navi = UINavigationController(rootViewController: RxViewController())
         self.window?.rootViewController = navi
         self.window?.makeKeyAndVisible()
         return true
---
+++ iOS/RxSwift_yamatatsu/RxSwiftPractice/RxViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RxViewController.swift
+//  RxSwiftPractice
+//
+//  Created by yamamoto.tatsuya on 2019/07/30.
+//  Copyright © 2019 yamamoto.tatsuya. All rights reserved.
+//
+
+import UIKit
+
+class RxViewController: UIViewController {
+
+    @IBOutlet weak var countLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func countUp(_ sender: Any) {
+        
+    }
+    
+    @IBAction func countDown(_ sender: Any) {
+        
+    }
+    
+    @IBAction func countReset(_ sender: Any) {
+        
+    }
+    
+}
