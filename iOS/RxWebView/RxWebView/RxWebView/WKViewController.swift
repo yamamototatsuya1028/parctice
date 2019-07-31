e86d03b3b2e4e7e51643e30606dcd7478d24f5e0
yamamototatsuya <yamamoto.tatsuya@xchange.jp>
2019-07-31 11:20:15 +0900

    fix webView

--- iOS/RxWebView/RxWebView/RxWebView/WKViewController.swift
+++ iOS/RxWebView/RxWebView/RxWebView/WKViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import WebKit
 
 class WKViewController: UIViewController {
 
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupWebView()
     }
     
+    private func setupWebView() {
+        webView.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+        
+        let url =  URL(string: "https://www.google.com")
+        let urlRequest = URLRequest(url: url!)
+        webView.load(urlRequest)
+        progressView.setProgress(0.1, animated: true)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "loading" {
+            UIApplication.shared
+                .isNetworkActivityIndicatorVisible = webView.isLoading
+            if !webView.isLoading {
+                progressView.setProgress(0.0, animated: false)
+                navigationItem.title = webView.title
+            }
+        }
+        
+        if keyPath == "estimatedProgress" {
+            progressView.setProgress(Float(webView.estimatedProgress), animated: true)
+        }
+    }
 
 }
